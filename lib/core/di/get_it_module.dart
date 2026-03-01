1adc524af192316a3a85edaa97677102865fa02f
Ahmed Nabil <ahmed.nabil@kazyon.com>
2026-03-01 21:28:45 +0200

    Add base example for retrofit - getIt

---
+++ lib/core/di/get_it_module.dart
@@ -0,0 +1,20 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:route_e_commerce_v2/core/constants/api_constants.dart';
+
+@module
+abstract class GetItModule {
+  @singleton
+  Connectivity createConnectivity() => Connectivity();
+
+  @singleton
+  Dio createDio() {
+    var dio = Dio(
+      BaseOptions(baseUrl: ApiConstants.baseUrl)
+    );
+    dio.interceptors.add(PrettyDioLogger());
+    return dio;
+  }
+}
