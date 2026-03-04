63b7e9e83681e22c35571a6239ce3082677b68a7
Ahmed Nabil <ahmed.nabil@kazyon.com>
2026-03-04 22:55:26 +0200

    Finished home tab.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:route_e_commerce_v2/core/routing/routes.dart';
 import 'package:route_e_commerce_v2/features/cart/presentation/screen/cart_screen.dart';
 import 'package:route_e_commerce_v2/features/auth/ui/screens/login/login.dart';
-import 'package:route_e_commerce_v2/features/navigation_layout/navigation_view.dart';
+import 'package:route_e_commerce_v2/features/commerce/ui/screens/navigation_layout/navigation_view.dart';
 
 abstract class AppRouter {
   static Route generateRoute(RouteSettings settings) {
